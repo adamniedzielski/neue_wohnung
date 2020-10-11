c9ce477e8a08fb29fecee7524b894bad8eebae6d
Adam Niedzielski <adamsunday@gmail.com>
2020-10-11 11:14:09 +0200

    Mock HTML interactions in test

--- spec/services/scrape_gewobag_spec.rb
+++ spec/services/scrape_gewobag_spec.rb
@@ -1,16 +1,30 @@
 require "rails_helper"
 
 RSpec.describe ScrapeGewobag do
-  it "gets multiple apartments" do
-    result = ScrapeGewobag.new.call
+  class MockHTTPClient
+    class Response
+      def body
+        File.read(Rails.root.join("spec", "fixtures", "gewobag.html"))
+      end
+    end
 
-    expect(result.size).to eq 7
+    def get(_url)
+      Response.new
+    end
+  end
+
+  it "gets multiple apartments" do
+    service = ScrapeGewobag.new(http_client: MockHTTPClient.new)
+    result = service.call
+
+    expect(result.size).to eq 8
   end
 
   it "gets apartment address" do
-    result = ScrapeGewobag.new.call
+    service = ScrapeGewobag.new(http_client: MockHTTPClient.new)
+    result = service.call
 
     expect(result.first.fetch(:address))
-      .to eq "\n\t\t\t\tCosmarweg 105, 13591 Berlin/Staaken\t\t\t"
+      .to eq "\n\t\t\t\tRichard-Münch-Str. 42, 13591 Berlin/Staaken\t\t\t"
   end
 end
