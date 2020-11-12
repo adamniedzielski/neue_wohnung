1e54ebfea5663761f0b99492b127ba2e4be269c0
Adam Niedzielski <adamsunday@gmail.com>
2020-11-12 18:51:28 +0100

    Run dummy WBM scraper in production (#8)

--- app/services/get_new_apartments.rb
+++ app/services/get_new_apartments.rb
@@ -2,7 +2,7 @@
 
 class GetNewApartments
   def initialize(
-    scrapers: [ScrapeGewobag.new],
+    scrapers: [ScrapeGewobag.new, ScrapeWbm.new],
     send_telegram_message: SendTelegramMessage.new
   )
     self.scrapers = scrapers
