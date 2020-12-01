ef262987576ff0249986d8011adef8ff9cb32d91
Adam Niedzielski <adamsunday@gmail.com>
2020-12-01 07:05:52 +0100

    Implement real WBM scraper (#7)
    * Add tests
    
    * Implement solution to tests
    
    * Improve code quality

--- app/services/scrape_wbm.rb
+++ app/services/scrape_wbm.rb
@@ -8,12 +8,27 @@ class ScrapeWbm
   end
 
   def call
-    _page = Nokogiri::HTML(http_client.get(URL).body)
-
-    []
+    page = Nokogiri::HTML(http_client.get(URL).body)
+    page.css(".openimmo-search-list-item").map { |listing| parse(listing) }
   end
 
   private
 
   attr_accessor :http_client
+
+  def parse(listing)
+    Apartment.new(
+      external_id: "wbm-#{listing.attribute('data-id').value}",
+      properties: {
+        address: listing.css(".address").text.split(",").join(", "),
+        url: listing.css(".btn.sign").attribute("href").value,
+        rooms_number: rooms_number(listing),
+        wbs: listing.css(".check-property-list").text.include?("WBS")
+      }
+    )
+  end
+
+  def rooms_number(listing)
+    Integer(listing.css(".main-property-list").text.match(/Zimmer:(\d*)/)[1])
+  end
 end
