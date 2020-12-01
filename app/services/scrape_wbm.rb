e101b91c3cf5a715b8b99cee61217abb10e9fa3b
Adam Niedzielski <adamsunday@gmail.com>
2020-12-01 07:01:13 +0100

    Improve code quality

--- app/services/scrape_wbm.rb
+++ app/services/scrape_wbm.rb
@@ -9,21 +9,26 @@ class ScrapeWbm
 
   def call
     page = Nokogiri::HTML(http_client.get(URL).body)
-
-    page.css(".openimmo-search-list-item").map do |listing|
-      Apartment.new(
-        external_id: "wbm-#{listing.attribute("data-id").value}",
-        properties: {
-          address: listing.css(".address").text.split(",").join(", "),
-          url: listing.css(".btn.sign").attribute("href").value,
-          rooms_number: Integer(listing.css(".main-property-list").text.match(/Zimmer:(\d*)/)[1]),
-          wbs: listing.css(".check-property-list").text.include?("WBS")
-        }
-      )
-    end
+    page.css(".openimmo-search-list-item").map { |listing| parse(listing) }
   end
 
   private
 
   attr_accessor :http_client
+
+  def parse(listing)
+    Apartment.new(
+      external_id: "wbm-#{listing.attribute('data-id').value}",
+      properties: {
+        address: listing.css(".address").text.split(",").join(", "),
+        url: listing.css(".btn.sign").attribute("href").value,
+        rooms_number: rooms_number(listing),
+        wbs: listing.css(".check-property-list").text.include?("WBS")
+      }
+    )
+  end
+
+  def rooms_number(listing)
+    Integer(listing.css(".main-property-list").text.match(/Zimmer:(\d*)/)[1])
+  end
 end
