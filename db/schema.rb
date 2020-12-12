f3d29f315165d10ddf917f1a7c34be00c09b133e
Adam Niedzielski <adamsunday@gmail.com>
2020-12-12 18:04:08 +0100

    Create receiver model

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_013_081_433) do
+ActiveRecord::Schema.define(version: 2020_12_12_170042) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,4 +21,15 @@ ActiveRecord::Schema.define(version: 20_201_013_081_433) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "receivers", force: :cascade do |t|
+    t.string "name"
+    t.string "telegram_chat_id"
+    t.boolean "include_wbs"
+    t.integer "minimum_rooms_number"
+    t.integer "maximum_rooms_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
