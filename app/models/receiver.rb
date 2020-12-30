91f95b7d5325721424843d3be38135fda1c9b6da
Adam Niedzielski <adamsunday@gmail.com>
2020-12-30 11:04:56 +0100

    Validate receiver preferences (#33)
    * Validate receiver preferences
    
    * Add database constraints
    
    * Add missing validation for Apartment model
    
    * Format code

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class Receiver < ApplicationRecord
+  validates :name, presence: true
+  validates :telegram_chat_id, presence: true
+  validates :include_wbs, inclusion: [true, false]
+  validates(
+    :minimum_rooms_number,
+    :maximum_rooms_number,
+    numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  )
 end
