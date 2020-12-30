f171d4d1394a72f87aa7ddaaba54f7a4da12e0f9
Adam Niedzielski <adamsunday@gmail.com>
2020-12-30 10:46:10 +0100

    Format code

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -4,5 +4,9 @@ class Receiver < ApplicationRecord
   validates :name, presence: true
   validates :telegram_chat_id, presence: true
   validates :include_wbs, inclusion: [true, false]
-  validates :minimum_rooms_number, :maximum_rooms_number, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates(
+    :minimum_rooms_number,
+    :maximum_rooms_number,
+    numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  )
 end
