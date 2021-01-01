0b825a0ea1c526f6acc40e6b1c42e554155c34d9
Adam Niedzielski <adamsunday@gmail.com>
2021-01-01 21:35:56 +0100

    Scrape Neues Berlin

---
+++ spec/services/scraper/neues_berlin_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "test_helpers/mock_http_client"
+
+RSpec.describe Scraper::NeuesBerlin do
+  it "gets multiple apartments" do
+    http_client = MockHTTPClient.new("neues_berlin.html")
+    service = Scraper::NeuesBerlin.new(http_client: http_client)
+    result = service.call
+
+    expect(result.size).to eq 1
+  end
+
+  it "returns Apartment instances" do
+    http_client = MockHTTPClient.new("neues_berlin.html")
+    service = Scraper::NeuesBerlin.new(http_client: http_client)
+    result = service.call
+
+    expect(result.first.class).to eq Apartment
+  end
+
+  it "gets apartment address" do
+    http_client = MockHTTPClient.new("neues_berlin.html")
+    service = Scraper::NeuesBerlin.new(http_client: http_client)
+    result = service.call
+
+    expect(result.first.properties.fetch("address"))
+      .to eq "Ahrenshooper Str. 38 13051 Berlin"
+  end
+
+  # it "assigns external identifier" do
+  #   http_client = MockHTTPClient.new("gewobag.html")
+  #   service = Scraper::Gewobag.new(http_client: http_client)
+  #   result = service.call
+
+  #   expect(result.first.external_id).to eq "gewobag-43925"
+  # end
+
+  # it "gets link to the full offer" do
+  #   http_client = MockHTTPClient.new("gewobag.html")
+  #   service = Scraper::Gewobag.new(http_client: http_client)
+  #   result = service.call
+
+  #   expect(result.first.properties.fetch("url"))
+  #     .to eq "https://www.gewobag.de/fuer-mieter-und-mietinteressenten/mietangebote/7100-74806-0305-0076/"
+  # end
+
+  # it "gets the number of rooms" do
+  #   http_client = MockHTTPClient.new("gewobag.html")
+  #   service = Scraper::Gewobag.new(http_client: http_client)
+  #   result = service.call
+
+  #   expect(result.first.properties.fetch("rooms_number"))
+  #     .to eq 3
+  # end
+
+  # it "gets the WBS status" do
+  #   http_client = MockHTTPClient.new("gewobag.html")
+  #   service = Scraper::Gewobag.new(http_client: http_client)
+  #   result = service.call
+
+  #   expect(result.first.properties.fetch("wbs"))
+  #     .to eq false
+  # end
+end
