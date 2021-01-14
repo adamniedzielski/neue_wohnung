d9ee044e4206c6217da4f612656711f02a683689
Adam Niedzielski <adamsunday@gmail.com>
2021-01-14 10:16:10 +0100

    Remove Bugsnag warning from EWG Pankow scraper

--- spec/services/scraper/ewg_pankow_spec.rb
+++ spec/services/scraper/ewg_pankow_spec.rb
@@ -11,31 +11,4 @@ RSpec.describe Scraper::EwgPankow do
 
     expect(result.size).to eq 0
   end
-
-  it "sends Bugnsnag notification when there's some change in HTML" do
-    http_client = MockHTTPClient.new("ewg_pankow_different.html")
-    bugnsnag_client = class_double(Bugsnag, notify: nil)
-    service = Scraper::EwgPankow.new(
-      http_client: http_client,
-      bugsnag_client: bugnsnag_client
-    )
-
-    service.call
-
-    expect(bugnsnag_client)
-      .to have_received(:notify).with(Scraper::EwgPankow::ContentChanged)
-  end
-
-  it "does not send Bugnsnag notification when there's a message about no flats" do
-    http_client = MockHTTPClient.new("ewg_pankow_empty.html")
-    bugnsnag_client = class_double(Bugsnag, notify: nil)
-    service = Scraper::EwgPankow.new(
-      http_client: http_client,
-      bugsnag_client: bugnsnag_client
-    )
-
-    service.call
-
-    expect(bugnsnag_client).not_to have_received(:notify)
-  end
 end
