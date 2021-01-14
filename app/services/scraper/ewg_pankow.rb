52490375cef0eaf83d02c8de2252fbe87652b9dd
Adam Niedzielski <adamsunday@gmail.com>
2021-01-14 10:19:50 +0100

    Remove Bugsnag warning from EWG Pankow scraper (#50)

--- app/services/scraper/ewg_pankow.rb
+++ app/services/scraper/ewg_pankow.rb
@@ -6,26 +6,19 @@ module Scraper
 
     URL = "https://www.ewg-pankow.de/wohnen/"
 
-    def initialize(http_client: HTTParty, bugsnag_client: Bugsnag)
+    def initialize(http_client: HTTParty)
       self.http_client = http_client
-      self.bugsnag_client = bugsnag_client
     end
 
     def call
       html = http_client.get(URL).body
-      page = Nokogiri::HTML(html)
-
-      unless page.text.include?("Aktuell ist leider kein Wohnungsangebot verfügbar")
-        bugsnag_client.notify(ContentChanged) do |report|
-          report.add_tab(:html, html: html)
-        end
-      end
+      _page = Nokogiri::HTML(html)
 
       []
     end
 
     private
 
-    attr_accessor :http_client, :bugsnag_client
+    attr_accessor :http_client
   end
 end
