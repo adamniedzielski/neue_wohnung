b89b103a44537d6718b42e3666fd6e58841aa1ae
Adam Niedzielski <adamsunday@gmail.com>
2021-04-19 10:47:07 +0200

    Implement Maerkische Scholle scraper

---
+++ app/services/scraper/maerkische_scholle.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Scraper
+  class MaerkischeScholle
+    URL = "https://www.maerkische-scholle.de/wohnungsangebote.html"
+
+    def initialize(http_client: HTTParty)
+      self.http_client = http_client
+    end
+
+    def call
+      page = Nokogiri::HTML(http_client.get(URL).body)
+      page.css("#main .ce_text").drop(1).map { |listing| parse(listing) }
+    end
+
+    private
+
+    attr_accessor :http_client
+
+    def parse(listing)
+      Apartment.new(
+        external_id: "maerkische-scholle-#{listing.css('a').attribute('href').value}",
+        properties: {
+          address: listing.css("p strong").first.children.first.text,
+          url: URL,
+          rooms_number: Integer(listing.css("h2").text.match(/(\d+)\sZimmer/)[1])
+        }
+      )
+    end
+  end
+end
---
+++ spec/services/scraper/maerkische_scholle_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "test_helpers/mock_http_client"
+
+RSpec.describe Scraper::MaerkischeScholle do
+  it "gets multiple apartments" do
+    http_client = MockHTTPClient.new("maerkische_scholle.html")
+    service = Scraper::MaerkischeScholle.new(http_client: http_client)
+    result = service.call
+
+    expect(result.size).to eq 2
+  end
+
+  it "returns Apartment instances" do
+    http_client = MockHTTPClient.new("maerkische_scholle.html")
+    service = Scraper::MaerkischeScholle.new(http_client: http_client)
+    result = service.call
+
+    expect(result.first.class).to eq Apartment
+  end
+
+  it "gets apartment address" do
+    http_client = MockHTTPClient.new("maerkische_scholle.html")
+    service = Scraper::MaerkischeScholle.new(http_client: http_client)
+    result = service.call
+
+    expect(result.first.properties.fetch("address"))
+      .to eq "Holtheimer Weg 30 (Lichterfelde-Süd)"
+  end
+
+  it "assigns external identifier" do
+    http_client = MockHTTPClient.new("maerkische_scholle.html")
+    service = Scraper::MaerkischeScholle.new(http_client: http_client)
+    result = service.call
+
+    expect(result.first.external_id)
+      .to eq "maerkische-scholle-files/bilder/content/wohungsangebote/2020/A3a_Holth.30_2_Zi_2.OGre.png"
+  end
+
+  it "links to the page with all offers" do
+    http_client = MockHTTPClient.new("maerkische_scholle.html")
+    service = Scraper::MaerkischeScholle.new(http_client: http_client)
+    result = service.call
+
+    expect(result.first.properties.fetch("url"))
+      .to eq "https://www.maerkische-scholle.de/wohnungsangebote.html"
+  end
+
+  it "gets the number of rooms" do
+    http_client = MockHTTPClient.new("maerkische_scholle.html")
+    service = Scraper::MaerkischeScholle.new(http_client: http_client)
+    result = service.call
+
+    expect(result.first.properties.fetch("rooms_number"))
+      .to eq 2
+  end
+end
