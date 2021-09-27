f4d894bfbdb124ce5b918962d3d11dab38d1ef9c
Adam Niedzielski <adamsunday@gmail.com>
2021-09-27 16:47:03 +0200

    Enable Charlotte scraper (#84)

--- app/services/scrape_all.rb
+++ app/services/scrape_all.rb
@@ -4,6 +4,7 @@ class ScrapeAll
   def initialize(
     scrapers: [
       Scraper::Bbg.new,
+      Scraper::Charlotte.new,
       Scraper::Degewo.new,
       Scraper::Dpf.new,
       Scraper::EwgPankow.new,
