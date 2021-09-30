3604bc6c5ca3d1d5610e7c3872c69bc737bd04d1
Adam Niedzielski <adam@AdamN-MBP.local>
2021-09-30 15:50:50 +0200

    Implement DetermineSubdistrict

--- app/services/determine_subdistrict.rb
+++ app/services/determine_subdistrict.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 class DetermineSubdistrict
+  SUBDISTRICTS_BY_ZIP_CODE = {
+    "10439" => "Prenzlauer Berg"
+  }.freeze
+
   def call(apartment)
-    apartment
+    match = apartment.properties.fetch("address", "").match(/\d{5}/)
+
+    return unless match
+
+    zip_code = match[0]
+    subdistrict = SUBDISTRICTS_BY_ZIP_CODE.fetch(zip_code, nil)
+
+    return unless subdistrict
+
+    apartment.properties["subdistrict"] = subdistrict
   end
 end
--- spec/services/determine_subdistrict_spec.rb
+++ spec/services/determine_subdistrict_spec.rb
@@ -14,4 +14,36 @@ RSpec.describe DetermineSubdistrict do
 
     expect(apartment.properties.fetch("subdistrict")).to eq "Prenzlauer Berg"
   end
+
+  it "does not assign subdistrict when the zip code is incorrect" do
+    apartment = Apartment.new(
+      properties: {
+        address: "Schönhauser Allee 107, 12345 Berlin"
+      }
+    )
+
+    DetermineSubdistrict.new.call(apartment)
+
+    expect(apartment.properties.key?("subdistrict")).to eq false
+  end
+
+  it "does not assign subdistrict when the zip code cannot be found in the address" do
+    apartment = Apartment.new(
+      properties: {
+        address: "Schönhauser Allee 107 Berlin"
+      }
+    )
+
+    DetermineSubdistrict.new.call(apartment)
+
+    expect(apartment.properties.key?("subdistrict")).to eq false
+  end
+
+  it "does not assign subdistrict when address is not available" do
+    apartment = Apartment.new(properties: {})
+
+    DetermineSubdistrict.new.call(apartment)
+
+    expect(apartment.properties.key?("subdistrict")).to eq false
+  end
 end
